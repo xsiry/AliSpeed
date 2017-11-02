bb5c7146b5811c1683b0a7472993ca84c3c352dc
xsiry <xsiries@gmail.com>
2017-11-02 16:21:11 +0800

    #个人资料查看及其修改功能 #个人密码修改功能

--- app/index/module.js
+++ app/index/module.js
@@ -6,9 +6,16 @@
  */
 define(function(require, exports, module) {
     var $ = require('jquery');
+    require('bootstrap');
+    require('jquery-confirm');
+    require('formValidation');
+    require('fvbootstrap');
+    require('fvzh_CN');
+    require('device');
+    require.async('toastr');
+
     $.index_ = $(document);
 
-    require('device');
     var click = device.mobile() ? 'touchstart' : 'click';
 
     module.exports = {
@@ -39,10 +46,27 @@ define(function(require, exports, module) {
             });
             $('body').attr('id', systemname);
             $('#system_title').text(systemtitle);
+            this._welcomeMsg();
+            this._setProfile();
             this._menusAjax(this, []);
             this._bindUI();
         },
         _bindUI: function() {
+            // 设置input特效
+            $(document).on('focus', 'input[type="text"]', function() {
+                $(this).parent().find('label').addClass('active');
+            }).on('blur', 'input[type="text"]', function() {
+                if ($(this).val() == '') {
+                    $(this).parent().find('label').removeClass('active');
+                }
+            });
+            $(document).on('focus', 'input[type="password"]', function() {
+                $(this).parent().find('label').addClass('active');
+            }).on('blur', 'input[type="password"]', function() {
+                if ($(this).val() == '') {
+                    $(this).parent().find('label').removeClass('active');
+                }
+            });
             // 侧边栏操作按钮
             $.index_.on(click, '#guide', function() {
                 $(this).toggleClass('toggled');
@@ -80,12 +104,32 @@ define(function(require, exports, module) {
                 initScrollState();
             });
 
+            // 全屏
+            $.index_.on('click', '.x-fullscreen', function() {
+                fullPage();
+            });
+
+            // 个人资料
+            $.index_.on('click', '.x-account', function() {
+                showAccount();
+            });
+
+            // 修改密码
+            $.index_.on('click', '.x-epassword', function() {
+                editPassword();
+            });
+
+            // 退出登录
+            $.index_.on('click', '.x-logout', function() {
+                logout();
+            });
+
             // 菜单点击
             $.index_.on('click', '.x-menu', function() {
                 var title = $(this).data('title');
                 var url = $(this).data('url');
                 if (title && url && url != '#') addTab(title, url);
-            })
+            });
 
             // 选项卡点击
             $.index_.on('click', '.content_tab li', function() {
@@ -144,10 +188,10 @@ define(function(require, exports, module) {
                     showMethod: 'slideDown',
                     timeOut: 4000
                 };
-                toastr.success('当前时间：' + getNowTime(), '欢迎进入 方格子●广告发布系统');
-            }, 1300);
+                toastr.success('当前时间：' + getNowTime(), '欢迎进入 ADMIN系统');
+            }, 1000);
         },
-        setProfile: function () {
+        _setProfile: function () {
             $.get('/user/info', {}, function(result) {
                 // $('ul#side-menu .profile_img').attr('src', user.img);
                 // $('ul#side-menu .profile_name').text(result.username);
@@ -234,7 +278,6 @@ define(function(require, exports, module) {
         }
     }
 
-
     function addTab(title, url) {
         console.log("Open Tab:=" + url);
         var x_content_height = document.documentElement.clientHeight - 118;
@@ -276,6 +319,279 @@ define(function(require, exports, module) {
         initScrollShow();
     }
 
+    function showAccount() {
+        var title = "个人资料";
+        $.get('/user/info', {}, function (result) {
+            $.confirm({
+                type: 'dark',
+                animationSpeed: 300,
+                title: title,
+                content: 'url:../app/personal_data_dialog.html',
+                buttons: {
+                    confirm: {
+                        text: '确认',
+                        type: 'submit',
+                        btnClass: 'waves-effect waves-button',
+                        action: function() {
+                            var self = this;
+                            self.$content.find('form').submit();
+                            return false;
+                        }
+                    },
+                    cancel: {
+                        text: '取消',
+                        btnClass: 'waves-effect waves-button'
+                    }
+                },
+                onOpen: function() {
+                    var self = this;
+                    setTimeout(function(result) {
+                        $.each(result, function(key, val) {
+                            self.$content.find('label[for="'+ key +'"]').addClass('active');
+                            self.$content.find('input[name="'+ key +'"]').val(val);
+                        });
+
+                        self.$content.find('form').formValidation({
+                            autoFocus: true,
+                            locale: 'zh_CN',
+                            message: '该值无效，请重新输入',
+                            err: {
+                                container: 'tooltip'
+                            },
+                            icon: {
+                                valid: 'glyphicon glyphicon-ok',
+                                invalid: 'glyphicon glyphicon-remove',
+                                validating: 'glyphicon glyphicon-refresh'
+                            },
+                            fields: {
+                                mobile: {
+                                    validators: {
+                                        notEmpty: {
+                                            message: '手机号不能为空'
+                                        },
+                                        regexp: {
+                                            regexp: /^1\d{10}$/,
+                                            message: '手机号格式不正确'
+                                        }
+                                    }
+                                },
+                                qq: {
+                                    validators: {
+                                        notEmpty: {
+                                            message: 'QQ不能为空'
+                                        },
+                                        regexp: {
+                                            regexp: /^[0-9]+$/,
+                                                message: 'QQ只能由纯数字组成'
+                                        }
+                                    }
+                                },
+                                email: {
+                                    validators: {
+                                        notEmpty: {
+                                            message: '邮箱不能为空'
+                                        },
+                                        regexp: {
+                                            regexp: /^([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+\.(?:com|cn)$/,
+                                            message: '邮箱格式不正确'
+                                        }
+                                    }
+                                }
+                            }
+                        }).on('success.form.fv', function (e) {
+                            $(self.$$confirm[0]).prop("disabled",true);
+                            // Prevent form submission
+                            e.preventDefault();
+
+                            // Get the form instance
+                            var $form = $(e.target);
+
+                            var params = {};
+
+                            $.each($form.serializeArray(), function (i, o) {
+                                params[o.name] = o.value;
+                            });
+
+                            $.post('/user/info', params, function (result) {
+                                var msg;
+                                toastr.options = {
+                                    closeButton: true,
+                                    progressBar: true,
+                                    showMethod: 'slideDown',
+                                    timeOut: 4000
+                                };
+                                if (result.success) {
+                                    msg = title+result.msg;
+                                    toastr.success(msg);
+                                    self.close();
+                                } else {
+                                    msg = title+result.msg;
+                                    toastr.error(msg);
+                                    $(self.$$confirm[0]).prop("disabled", false);
+                                };
+
+                            }, 'json');
+                        });
+                    }, 500, result);
+                }
+            });
+        }, 'json')
+    }
+
+    function editPassword() {
+        var title = "密码修改";
+        $.confirm({
+            type: 'red',
+            animationSpeed: 300,
+            title: title,
+            content: 'url:../app/epassword_dialog.html',
+            buttons: {
+                confirm: {
+                    text: '确认',
+                    btnClass: 'waves-effect waves-button',
+                    action: function() {
+                        var self = this;
+                        self.$content.find('form').submit();
+                        return false;
+                    }
+                },
+                cancel: {
+                    text: '取消',
+                    btnClass: 'waves-effect waves-button'
+                }
+            },
+            onOpen: function() {
+                var self = this;
+                setTimeout(function() {
+                    self.$content.find('form').formValidation({
+                        autoFocus: true,
+                        locale: 'zh_CN',
+                        message: '该值无效，请重新输入',
+                        err: {
+                            container: 'tooltip'
+                        },
+                        icon: {
+                            valid: 'glyphicon glyphicon-ok',
+                            invalid: 'glyphicon glyphicon-remove',
+                            validating: 'glyphicon glyphicon-refresh'
+                        },
+                        fields: {
+                            old_password: {
+                                validators: {
+                                    notEmpty: {
+                                        message: '旧密码不能为空'
+                                    },
+                                    stringLength: {
+                                        min: 3,
+                                        max: 16,
+                                        message: '密码长度必须在3~16之间'
+                                    },
+                                    regexp: {
+                                        regexp: /^[a-zA-Z0-9]+$/,
+                                        message: '密码只能由大小写字母和数字组成'
+                                    }
+                                }
+                            },
+                            new_password: {
+                                validators: {
+                                    notEmpty: {
+                                        message: '新密码不能为空'
+                                    },
+                                    stringLength: {
+                                        min: 3,
+                                        max: 16,
+                                        message: '密码长度必须在3~16之间'
+                                    },
+                                    regexp: {
+                                        regexp: /^[a-zA-Z0-9]+$/,
+                                        message: '密码只能由大小写字母和数字组成'
+                                    },
+                                    identical: {
+                                        field: 'confirm_password',
+                                        message: '新密码与确认密码不一致'
+
+                                    },
+                                }
+                            },
+                            confirm_password: {
+                                validators: {
+                                    notEmpty: {
+                                        message: '验证密码不能为空'
+                                    },
+                                    stringLength: {
+                                        min: 3,
+                                        max: 16,
+                                        message: '密码长度必须在3~16之间'
+                                    },
+                                    regexp: {
+                                        regexp: /^[a-zA-Z0-9]+$/,
+                                        message: '密码只能由大小写字母和数字组成'
+                                    },
+                                    identical: {
+                                        field: 'new_password',
+                                        message: '新密码与确认密码不一致'
+
+                                    }
+                                }
+                            }
+                        }
+                    }).on('success.form.fv', function (e) {
+                        $(self.$$confirm[0]).prop("disabled",true);
+                        // Prevent form submission
+                        e.preventDefault();
+
+                        // Get the form instance
+                        var $form = $(e.target);
+
+                        var params = {};
+
+                        $.each($form.serializeArray(), function (i, o) {
+                            params[o.name] = o.value;
+                        });
+
+                        $.post('/user/epassword', params, function (result) {
+                            var msg;
+                            toastr.options = {
+                                closeButton: true,
+                                progressBar: true,
+                                showMethod: 'slideDown',
+                                timeOut: 4000
+                            };
+                            if (result.success) {
+                                msg = title+result.msg;
+                                toastr.success(msg);
+                                self.close();
+                            } else {
+                                msg = title+result.msg;
+                                toastr.error(msg);
+                                $(self.$$confirm[0]).prop("disabled", false);
+                            };
+                        }, 'json');
+                    });
+                }, 500);
+            }
+        });
+    }
+
+    function logout() {
+        $.get('/user/logout', {}, function (result) {
+            var msg;
+            toastr.options = {
+                closeButton: true,
+                progressBar: true,
+                showMethod: 'slideDown',
+                timeOut: 4000
+            };
+            if (result.success) {
+                toastr.success(result.msg);
+                window.location = 'login.html';
+            } else {
+                msg = "退出登录失败！";
+                toastr.error(msg);
+            }
+            ;
+        }, 'json')
+    }
     // 选项卡右键菜单
     require('BootstrapMenu');
     var menu = new BootstrapMenu('.tabs li', {
@@ -388,5 +704,29 @@ define(function(require, exports, module) {
             },
             "async": !0
         })
+    }
+
+    /**
+     * 获取当前时间
+     */
+    function getNowTime() {
+
+        function p(s) {
+            return s < 10 ? '0' + s : s;
+        }
+
+        var myDate = new Date();
+        //获取当前年
+        var year = myDate.getFullYear();
+        //获取当前月
+        var month = myDate.getMonth() + 1;
+        //获取当前日
+        var date = myDate.getDate();
+        var h = myDate.getHours(); //获取当前小时数(0-23)
+        var m = myDate.getMinutes(); //获取当前分钟数(0-59)
+        var s = myDate.getSeconds();
+
+        var now = [year, p(month), p(date)].join('-') + " " + [p(h), p(m), p(s)].join(':');
+        return now;
     }
 })
