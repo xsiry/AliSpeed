7f05f280b2831fe6ca69edf286b1e671acce20fc
xsiry <xsiries@gmail.com>
2017-11-22 13:41:41 +0800

    #终端权益走势图

--- app/index/module_myindex.js
+++ app/index/module_myindex.js
@@ -76,124 +76,92 @@ define(function (require, exports, module) {
     }
 
     function initChart() {
-        Highcharts.chart('mac_container', {
-            title: {
-                text: null
-            },
-            yAxis: {
+        $.get('/rep_days_agent_mac', {}, function(result) {
+            var option = {
+                title: {
+                    text: null
+                },
+                xAxis: {
+                    type: 'datetime',
+                        dateTimeLabelFormats: {
+                        day: '%m月%d日',
+                            week: '%m月%d日',
+                            month: '%Y年%m月',
+                            year: '%Y年'
+                    },
+                    labels: {
+                        rotation: -45
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    series: {
+                        label: {
+                            connectorAllowed: false
+                        }
+                    }
+                },
+                    responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            legend: {
+                                layout: 'horizontal',
+                                align: 'center',
+                                verticalAlign: 'bottom'
+                            }
+                        }
+                    }]
+                },
+                credits: { enabled: false }
+            };
+
+            var mac = [];
+            var equity = [];
+            $.each(result.data, function(i, o) {
+                var split = o.days.split('-');
+                var date = Date.UTC(split[0], split[1], split[2]);
+                mac.push([date, o.total_mac.toFloat()]);
+                equity.push([date, o.total_profit.toFloat()]);
+            });
+
+            option['yAxis'] = {
                 title: {
                     text: '终端(台)'
                 }
-            },
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: {
-                    day: '%m月%d日',
-                    week: '%m月%d日',
-                    month: '%Y年%m月',
-                    year: '%Y年'
-                },
-                labels: {
-                    rotation: -45
-                }
-            },
-            tooltip: {
+            };
+            option['series'] = [{
+                name: '终端数',
+                data: mac
+            }];
+
+            option['tooltip'] = {
                 headerFormat: '<b>日期：</b>{point.x:%Y年%m月%d日}<br>',
                 pointFormat: '<b>终端：</b>{point.y:.0f}台'
-            },
-            legend: {
-                enabled: false
-            },
-            series: [{
-                name: '终端数',
-                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            },
-            credits: { enabled: false } //不显示LOGO
-        });
+            };
 
-        Highcharts.chart('equity_container', {
-            title: {
-                text: null
-            },
-            yAxis: {
+            Highcharts.chart('mac_container', option);
+
+            option['yAxis'] = {
                 title: {
                     text: '权益(元)'
                 }
-            },
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: {
-                    day: '%m月%d日',
-                    week: '%m月%d日',
-                    month: '%Y年%m月',
-                    year: '%Y年'
-                },
-                labels: {
-                    rotation: -45
-                }
-            },
-            tooltip: {
+            };
+            option['series'] = [{
+                name: '权益值',
+                data: equity
+            }];
+
+            option['tooltip'] = {
                 headerFormat: '<b>日期：</b>{point.x:%Y年%m月%d日}<br>',
                 pointFormat: '<b>权益：</b>{point.y:.2f}元'
-            },
-            legend: {
-                enabled: false
-            },
-            plotOptions: {
-                series: {
-                    label: {
-                        connectorAllowed: false
-                    }
-                }
-            },
-            series: [{
-                name: '权益值',
-                data: [{
-                    x: Date.UTC(2013,5,2),
-                    y: 0.7648,
-                    name: "Point2",
-                    color: "#00FF00"
-                }, {
-                    x: Date.UTC(2013,5,4),
-                    y: 0.7645,
-                    name: "Point1",
-                    color: "#FF00FF"
-                }, {
-                    x: Date.UTC(2013,5,5),
-                    y: 0.7645,
-                    name: "Point1",
-                    color: "#FF00FF"
-                }]
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            },
-            credits: { enabled: false }
-        });
+            };
+
+            Highcharts.chart('equity_container', option);
+        }, 'json');
     }
 });
